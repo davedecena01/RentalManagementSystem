fb1ee0eb32059f25d909a3077f603ce14566f4cf
Dave Decena <davedecena01@gmail.com>
2026-05-13 04:29:56 +0800

    feat(provisions): guard provision-templates route with landlordGuard

--- frontend/rental-management-ui/src/app/features/account/account.routes.ts
+++ frontend/rental-management-ui/src/app/features/account/account.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { landlordGuard } from '../../core/guards/auth.guard';
 
 export const accountRoutes: Routes = [
   {
     path: 'provision-templates',
+    canActivate: [landlordGuard],
     loadComponent: () =>
       import('./provision-templates/provision-templates.component')
         .then(m => m.ProvisionTemplatesComponent)
